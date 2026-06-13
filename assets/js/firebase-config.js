10a183d891d62724d49c8f3c502a0c2bfb2d3cec
Luis <133532790+luissolutions@users.noreply.github.com>
2026-06-13 01:11:27 -0400

    Wire up custom domain + self-contained Firebase
    - Add CNAME for jeautocareservice.com
    - Sync latest jem source (canonical, og:url)
    - Inline visit logger in index.html (no cross-repo relative imports)
    - Copy firebase-init.js + firebase-config.js into assets/js/ so dash/img
      pages can authenticate without depending on luissolutions.github.io
    - Fix all firebase-init.js import paths to use ./assets/js/

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCv2cQGWeXS-w7psrQiZD8dn4R7hStmY1o",
+  authDomain: "persinfo-df93f.firebaseapp.com",
+  databaseURL: "https://persinfo-df93f-default-rtdb.firebaseio.com",
+  projectId: "persinfo-df93f",
+  storageBucket: "persinfo-df93f.appspot.com",
+  messagingSenderId: "218680336647",
+  appId: "1:218680336647:web:7786091136b9e6b28565a2"
+};
+
+export { firebaseConfig };
